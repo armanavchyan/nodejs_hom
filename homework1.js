4c079e68bdcb947f1e3c77fc01413253df1bb525
Arman <avchyanarman@gmail.com>
2021-03-27 19:13:15 +0400

    tnayin les22 kisat

--- homework1.js
+++ homework1.js
@@ -1,7 +1,11 @@
-// const http = require('http');
-// const server = http.createServer((req, res) => {
-//     if (req.url === '/sunny' ){
-//         console.log('YES')
-//     }
-// })
-// server.listen(3000);
+const fs = require('fs');
+const { EventEmitter } = require('events');
+const homework = new EventEmitter();
+
+const readFileTxt = fs.readFileSync('./Homeworks.txt', "utf-8");
+
+homework.on('data', (a) => {
+    console.log(a);
+});
+
+homework.emit('data',readFileTxt);
